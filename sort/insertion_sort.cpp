88e92cc2bee07d443c1e49f32120e7e5f9e670b0
nvbao117 <baocute056@gmail.com>
2024-08-19 19:52:19 +0700

    Insertion Sort

---
+++ sort/insertion_sort.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 24, 44, 90};
+    int size, safe, poi, shift;
+    size = sizeof(arr) / sizeof(arr[0]);
+    for (int addon = 1; addon < size; addon++)
+    {
+        for (poi = 0; arr[addon] > arr[poi]; poi++)
+            ;
+        safe = arr[addon];
+        for (shift = addon - 1; shift >= poi; shift--)
+        {
+            arr[shift + 1] = arr[shift];
+        }
+        arr[poi] = safe;
+    }
+
+    for (int ind = 0; ind < size; ind++)
+        cout << arr[ind] << " ";
+    return 0;
+}
